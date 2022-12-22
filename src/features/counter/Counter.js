d59ec7cc225094b3c488f590a1da6084c772d4b4
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-12-22 12:36:31 +0600

    Practice Done

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, reset, incrementByAmount } from './counterSlice';
+
+export const Counter = () => {
+	const count = useSelector((state) => state.counter.count);
+	const dispatch = useDispatch();
+	const [incrementAmount, setIncrementAmount] = useState(0);
+	const addValue = Number(incrementAmount) || 0;
+	const resetAll = () => {
+		setIncrementAmount(0);
+		dispatch(reset());
+	};
+
+	return (
+		<div>
+			<div>
+				<button
+					aria-label='Increment count'
+					onClick={() => dispatch(increment())}
+					onChange={(e) => setIncrementAmount(e.target.value)}>
+					Increment
+				</button>
+				<p>{count}</p>
+				<button
+					aria-label='Decrement count'
+					onClick={() => dispatch(decrement())}>
+					Decrement
+				</button>
+				<br />
+				<input
+					type='text'
+					value={incrementAmount}
+					onChange={(e) => setIncrementAmount(e.target.value)}
+				/>
+				<div>
+					<button
+						aria-label='Increment by amount'
+						onClick={() => dispatch(incrementByAmount(addValue))}>
+						Add Amount
+					</button>
+				</div>
+				<br />
+				<button aria-label='Reset count' onClick={resetAll}>
+					Reset
+				</button>
+			</div>
+		</div>
+	);
+};
---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+	count: 0,
+};
+
+export const counterSlice = createSlice({
+	name: 'counter',
+	initialState,
+	reducers: {
+		increment: (state) => {
+			// Redux Toolkit allows us to write "mutating" logic in reducers. It
+			// doesn't actually mutate the state because it uses the Immer library,
+			// which detects changes to a "draft state" and produces a brand new
+			// immutable state based off those changes
+			state.count += 1;
+		},
+		decrement: (state) => {
+			state.count -= 1;
+		},
+		reset: (state) => {
+			state.count = 0;
+		},
+		incrementByAmount: (state, action) => {
+			state.count += action.payload;
+		},
+	},
+});
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, reset, incrementByAmount } =
+	counterSlice.actions;
+
+export default counterSlice.reducer;
